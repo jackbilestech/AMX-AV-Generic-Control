0651c96b57d58950d21baaa5a543f3b492d6f800
Jack <jackbiles21@hotmail.co.uk>
2020-02-08 18:14:33 +0000

    update gitignore

---
+++ build/precache-manifest.7e38e0e20aab8aced24d24f440dc5a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322edd14947edf6af3f59734e0e4d4ca",
+    "url": "/AMX-AV-Generic-Control/index.html"
+  },
+  {
+    "revision": "a9abc7cfb01377157bdf",
+    "url": "/AMX-AV-Generic-Control/static/css/main.f159de91.chunk.css"
+  },
+  {
+    "revision": "990fe1326fb3b32e3524",
+    "url": "/AMX-AV-Generic-Control/static/js/2.5bbe9f82.chunk.js"
+  },
+  {
+    "revision": "61460a9918638f89da762dcefb150768",
+    "url": "/AMX-AV-Generic-Control/static/js/2.5bbe9f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9abc7cfb01377157bdf",
+    "url": "/AMX-AV-Generic-Control/static/js/main.f34a481d.chunk.js"
+  },
+  {
+    "revision": "262026c02dac3e7bc58d",
+    "url": "/AMX-AV-Generic-Control/static/js/runtime-main.0ad42ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AMX-AV-Generic-Control/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/AMX-AV-Generic-Control/precache-manifest.7e38e0e20aab8aced24d24f440dc5a23.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AMX-AV-Generic-Control/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
